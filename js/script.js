ec212018a26fce0c362813493f34ff674ab10286
dejvq <sliwinskidawid12@gmail.com>
2018-07-31 21:09:32 +0200

    Styled msw

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
 // Custom JS
 
 let mapContainer = document.querySelector(".main__map-container");
-let buttonMap = document.querySelector(".button__map")
+let buttonMap = document.querySelector(".button__map");
+let buttonMapClose = document.querySelector(".button__map--close");
 
 buttonMap.addEventListener("click", (e) => {
-    console.log(mapContainer.style.right);
-    
-    if (mapContainer.style.right = "100%") {
-        console.log("dsadsadsa12");
-         mapContainer.style.right = 0;
-    }
-    else {
-        alert("dasdsa");
-        mapContainer.style.right = "100px"
-    }
-    
-    if()
+        mapContainer.style.right = 0;
+        buttonMap.style.display = "none";
+});
+
+buttonMapClose.addEventListener("click", (e) => {
+    mapContainer.style.right = "105%";
 });
